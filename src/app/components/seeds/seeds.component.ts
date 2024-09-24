51e750ae55fa7d9a0c010f8bd0beaeaae6ec57e2
sriramv.genpro@gmail.com <sriramv.genpro@gmail.com>
2024-09-24 14:56:30 +0530

    updated sriram

--- src/app/components/seeds/seeds.component.ts
+++ src/app/components/seeds/seeds.component.ts
@@ -8,4 +8,42 @@ import { Component } from '@angular/core';
 export class SeedsComponent {
 ruppee:number=350;
 orgRuppee:number=700;
+seeds:any[]=[
+  {
+    title:"SAAHO TAMATO SEEDS",
+    category:"Sygenta",
+    ruppee:350,
+    orgRuppee:700,
+    img:"https://cdn.shopify.com/s/files/1/0722/2059/products/Sahoo.jpg?v=1671198625&width=256"
+  },
+  {
+    title:"SAAHO TAMATO SEEDS",
+    category:"Sygenta",
+    ruppee:350,
+    orgRuppee:700,
+    img:"https://cdn.shopify.com/s/files/1/0722/2059/products/Sahoo.jpg?v=1671198625&width=256"
+  },
+  {
+    title:"SAAHO TAMATO SEEDS",
+    category:"Sygenta",
+    ruppee:350,
+    orgRuppee:700,
+    img:"https://cdn.shopify.com/s/files/1/0722/2059/products/Sahoo.jpg?v=1671198625&width=256"
+  },
+  {
+    title:"SAAHO TAMATO SEEDS",
+    category:"Sygenta",
+    ruppee:350,
+    orgRuppee:700,
+    img:"https://cdn.shopify.com/s/files/1/0722/2059/products/Sahoo.jpg?v=1671198625&width=256"
+  },
+  {
+    title:"SAAHO TAMATO SEEDS",
+    category:"Sygenta",
+    ruppee:350,
+    orgRuppee:700,
+    img:"https://cdn.shopify.com/s/files/1/0722/2059/products/Sahoo.jpg?v=1671198625&width=256"
+  },
+
+];
 }
