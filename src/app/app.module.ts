3051b7cfa59b96be3a3bec76ddf2f884f9d36159
Karakambakam Chaithanya <chaitanyakarakambakam>
2024-09-28 10:46:53 +0530

    add a prod component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsComponent } from './components/products/products.component';
+import { ProdComponent } from './components/prod/prod.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProdComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/prod/prod.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prod',
+  templateUrl: './prod.component.html',
+  styleUrls: ['./prod.component.css']
+})
+export class ProdComponent {
+//   prod: any[] = [
+//     {
+//       title:"dgd",
+//       category: 'Product 1',
+//       rupee: 350,
+//       orgRupee: 29.99,
+//       image: 'https://www.bighaat.com/_next/image?url=https%3A%2F%2Fbighaat-prodv2-media-cdn.azureedge.net%2Ftrustmarkers%2Ftm_home_2_page_v2.webp&w=1200&q=75'
+//     },
+//     {
+//       id: 2,
+//       name: 'Product 2',
+//       description: 'Description for Product 2',
+//       price: 39.99,
+//       imageUrl: 'https://via.placeholder.com/150'
+//     },
+//     {
+//       id: 3,
+//       name: 'Product 3',
+//       description: 'Description for Product 3',
+//       price: 49.99,
+//       imageUrl: 'https://via.placeholder.com/150'
+//     }
+//     // Add more products as needed
+//   ];
+
+
+prod: any[] = [
+  {
+    title: 'BALWAAN BS-20 KRISHI..',
+    category: 'Modish Tractoraurkisan Pvt Ltd',
+    rupee: 3500,
+    orgRupes : 5000,
+    discout:1500,
+    img: 'https://cdn.shopify.com/s/files/1/0722/2059/products/1_01aafc8a-9703-4a0a-bd31-ec68c9606248.jpg?v=1680694282&width=256',
+  },
+  {
+    title: 'BALWAAN BS-22 KRISHI..',
+    category: 'Modish Tractoraurkisan Pvt Ltd',
+    rupee: 3990,
+    orgRupes : 1500,
+    discout:1010,
+    img: 'https://cdn.shopify.com/s/files/1/0722/2059/products/1_f289df75-56d2-43a7-8e58-c004899a1b4d.jpg?v=1682597440&width=256',
+  },
+  {
+    title: 'BALWAAN BS-21 2IN1..',
+    category: 'Modish Tractoraurkisan Pvt Ltd',
+    rupee: 3550,
+    orgRupes : 5000,
+    discout:1450,
+    img: 'https://cdn.shopify.com/s/files/1/0722/2059/files/1_2489a9ef-d924-4979-9dcd-83a394face29.webp?v=1684479981&width=256',
+  },
+  {
+    title: 'TAPAS PAHALWAAN-101...',
+    category: 'Modish Tractoraurkisan Pvt Ltd',
+    ruppee: 2579,
+    orgRupes : 4999,
+    discout:2420,
+    img: 'https://cdn.shopify.com/s/files/1/0722/2059/files/TAPASPAHALWAAN-101SINGLEMOTORBATTERYSPRAYER-12X8_PAGE-1.jpg?v=1711095110&width=256',
+  },
+  {
+    title: 'TAPAS PAHALWAAN 102..',
+    category: 'Modish Tractoraurkisan Pvt Ltd',
+    rupee: 2699,
+    orgRupes : 4999,
+    discout:2300,
+    img: 'https://cdn.shopify.com/s/files/1/0722/2059/files/21316a1d-0fa0-44a9-afc2-def6bd0d5ee4.jpg?v=1708413777&width=256',
+    },
+  ];
+
+}
+
