2e4e236b990349471da17d5763c06cdb9962093a
unknown <Maheshpadishala@accumenta.co.in>
2024-10-03 10:06:04 +0530

    cart component and order component added

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AddtocartService } from 'src/app/_services/addtocart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+
+  products:any[]=[]
+  totalAmount:number=0
+
+
+  constructor(private cartServ:AddtocartService) { }
+
+  ngOnInit(): void {
+    this.cartServ.getProductData().subscribe(res=>{
+      // console.log(res, "resss data");
+      this.products=res;
+      this.totalAmount=this.cartServ.getTotalAmount()
+      // console.log(res,this.totalAmount,"ress data");
+      
+    })
+  }
+
+  removeItem(item:any){
+    this.cartServ.removeCartData(item);
+
+  }
+  
+
 
 }
