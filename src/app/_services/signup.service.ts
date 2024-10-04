0a2b35d5ae1aef34e82a751adfa7d4787b8d3b6d
sriramv.genpro@gmail.com <sriramv.genpro@gmail.com>
2024-10-04 09:54:04 +0530

    signup errors clear

--- src/app/_services/signup.service.ts
+++ src/app/_services/signup.service.ts
@@ -14,10 +14,6 @@ export class SignupService {
   constructor(private http: HttpClient) {}
 
   createCustomer(data: any): Observable<any> {
-    let headers = new HttpHeaders().set(
-      'Authorization',
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6InNhaSBrdW1hciIsInBob25lIjoiOTU1MDI4NDMxOSIsImlhdCI6MTcyNzk2MjU1NywiZXhwIjoxNzI3OTczMzU3fQ.MrBvDW9G0H47X76yE6S2Q73mI7as_TIpgkgOdC6P-FU'
-    );
-    return this.http.post(`${url}employees/add-employee`,data,{ headers });
+    return this.http.post(`${url}employees/add-employee`,data);
   }
 }
