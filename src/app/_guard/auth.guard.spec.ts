e0138ab48e37aeca2e91093e12cb52a223761f8a
saikumar <sai.kumar@ittstar.com>
2024-10-05 18:52:43 +0530

    push

--- src/app/_guard/auth.guard.spec.ts
+++ src/app/_guard/auth.guard.spec.ts
@@ -3,6 +3,7 @@ import { CanActivateFn } from '@angular/router';
 
 import { authGuard } from './auth.guard';
 
+
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
